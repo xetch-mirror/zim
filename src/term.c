b9e76a65f037d62ca7ca417feeb8312ed2b361c9
Xetch <babyroselegaspi01@gmail.com>
2026-08-21 08:43:56 +0000

    BETA 1.0

--- src/term.c
+++ src/term.c
@@ -193,29 +193,6 @@ void term_print_at(int x, int y, int attr, const char *fmt, ...)
     va_end(args);
 }
 
-void term_vprint_bound_at(bound_t *bound, int x, int y, int attr,
-                           const char *fmt, va_list args)
-{
-    char buf[512];
-    int max_len;
-
-    vsnprintf(buf, sizeof(buf), fmt, args);
-
-    max_len = bound->x + bound->width - x;
-    if (max_len < 0)
-        return;
-    if ((int)strlen(buf) > max_len)
-        buf[max_len] = '\0';
-
-    if (y < bound->y || y >= bound->y + bound->height)
-        return;
-
-    term_goto(x, y);
-    apply_attr(attr);
-    term_write_str(buf);
-    term_reset_color();
-}
-
 void term_redraw(void)
 {
     /* пока без явной буферизации; позже можно добавить
